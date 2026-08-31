c8d12f378be5adfdfb18dcaafee4adca48e3fe57
Gaber_Anwar <gaberalkholy053@gmail.com>
2026-08-31 10:09:02 +0300

    Add SubscriptionStatus enum

---
+++ Backend/Shaghaf.Domain/Enums/SubscriptionStatus.cs
@@ -0,0 +1,12 @@
+namespace Shaghaf.Domain.Enums;
+
+/// <summary>
+/// Subscription status
+/// </summary>
+public enum SubscriptionStatus
+{
+    Active = 1,
+    Cancelled = 2,
+    Expired = 3,
+    Suspended = 4
+}
